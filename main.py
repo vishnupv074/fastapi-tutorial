bbf4b94b77cf00d3f08bc9cb352127a71d3c1578
vishnupv074 <vishnupv074@gmail.com>
2024-12-29 23:34:37 +0530

    FastAPI CRUD operation with filters - Book Class - BaseModel for BookRequest(pydantic validation) - APIs

---
+++ main.py
@@ -0,0 +1,225 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: float
+    published_year: int
+
+    def __init__(self, id, title, author, description, rating, published_year):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_year = published_year
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(
+        description="No need to pass ID on creation", default=None
+    )
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: float = Field(ge=0, le=5)
+    published_year: int = Field(ge=0)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "To Kill a Mockingbird",
+                "author": "Harper Lee",
+                "description": (
+                    "A novel set in the American South during the"
+                    " 1930s, focusing on themes of"
+                    "r acial injustice and moral growth."
+                ),
+                "rating": 4.8,
+                "published_year": 2012,
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(
+        **{
+            "id": 1,
+            "title": "To Kill a Mockingbird",
+            "author": "Harper Lee",
+            "description": (
+                "A novel set in the American South during the"
+                " 1930s, focusing on themes of racial injustice"
+                " and moral growth."
+            ),
+            "rating": 4.8,
+            "published_year": 2010,
+        }
+    ),
+    Book(
+        **{
+            "id": 2,
+            "title": "1984",
+            "author": "George Orwell",
+            "description": (
+                "A dystopian novel depicting a totalitarian regime"
+                " that uses surveillance and propaganda to control"
+                " its citizens."
+            ),
+            "rating": 4.7,
+            "published_year": 2012,
+        }
+    ),
+    Book(
+        **{
+            "id": 3,
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald",
+            "description": (
+                "A story about the enigmatic Jay Gatsby and his"
+                " unrequited love for Daisy Buchanan, set in the"
+                " Roaring Twenties."
+            ),
+            "rating": 4.6,
+            "published_year": 2010,
+        }
+    ),
+    Book(
+        **{
+            "id": 4,
+            "title": "Pride and Prejudice",
+            "author": "Jane Austen",
+            "description": (
+                "A romantic novel that explores the themes of "
+                "love, class, and societal expectations through"
+                " the story of Elizabeth Bennet and Mr. Darcy."
+            ),
+            "rating": 4.9,
+            "published_year": 2012,
+        }
+    ),
+    Book(
+        **{
+            "id": 5,
+            "title": "The Catcher in the Rye",
+            "author": "J.D. Salinger",
+            "description": (
+                "A coming-of-age novel following the experiences "
+                "of Holden Caulfield, a disillusioned teenager in"
+                " New York City."
+            ),
+            "rating": 4.4,
+            "published_year": 2012,
+        }
+    ),
+    Book(
+        **{
+            "id": 6,
+            "title": "The Hobbit",
+            "author": "J.R.R. Tolkien",
+            "description": (
+                "A fantasy novel about Bilbo Baggins' adventurous "
+                "journey to reclaim a treasure guarded by the "
+                "dragon Smaug."
+            ),
+            "rating": 4.8,
+            "published_year": 2009,
+        }
+    ),
+    Book(
+        **{
+            "id": 7,
+            "title": "The Alchemist",
+            "author": "Paulo Coelho",
+            "description": (
+                "A philosophical tale about Santiago, a shepherd "
+                "boy, who embarks on a journey to find a treasure,"
+                " learning about the importance of following one's"
+                " dreams."
+            ),
+            "rating": 4.7,
+            "published_year": 2020,
+        }
+    ),
+    Book(
+        **{
+            "id": 8,
+            "title": "Brave New World",
+            "author": "Aldous Huxley",
+            "description": (
+                "A dystopian novel set in a future society "
+                "characterized by technological advancements and "
+                "the loss of individuality."
+            ),
+            "rating": 4.5,
+            "published_year": 2012,
+        }
+    ),
+]
+
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    return {"message": "Book not found!"}
+
+
+@app.get("/books/")
+async def read_book_by_filtering(
+    rating: float | None = None, published_year: int | None = None
+):
+    print(rating, published_year)
+    filtered_books = BOOKS
+    if rating:
+        filtered_books = [
+            book for book in filtered_books if book.rating == rating
+        ]
+    if published_year:
+        filtered_books = [
+            book
+            for book in filtered_books
+            if book.published_year == published_year
+        ]
+    return filtered_books
+
+
+@app.post("/create-book")
+async def create_book(new_book: BookRequest):
+    book_obj = Book(**new_book.model_dump())
+    BOOKS.append(find_book_id(book_obj))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if not BOOKS else BOOKS[-1].id + 1
+    return book
+
+
+@app.put("/books/update_book")
+async def update_book(update_book: BookRequest):
+    for i, book in enumerate(BOOKS):
+        if book.id == update_book.id:
+            BOOKS[i] = update_book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i, book in enumerate(BOOKS):
+        if book.id == book_id:
+            BOOKS.pop(i)
+            break
